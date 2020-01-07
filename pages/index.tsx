fd290986c9c85e0e80709b0958dd6897182d694a
“Son <son.chau@geografia.com.au>
2020-01-08 10:48:02 +1100

    [#1] add react, typescript and next

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+interface InitialProps {
+    greeting: string
+}
+
+interface Props extends InitialProps {
+
+}
+
+const IndexPage: NextPage<Props, InitialProps> = (props) => {
+return <div> {props.greeting}</div>
+}
+
+IndexPage.getInitialProps = async () => ({
+    greeting: 'hi Son'
+})
+export default IndexPage
