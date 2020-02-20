7a8258f1e12a46df470c24ed8d2af65a68ab29e3
“Son <son.chau@geografia.com.au>
2020-02-20 11:31:22 +1100

    [#35] add option help content

--- components/quick/task/DrawerContent.tsx
+++ components/quick/task/DrawerContent.tsx
@@ -4,7 +4,21 @@ import React from 'react';
 const DrawerContent = () => {
     return (
         <div className="drawer-container">
-            <strong>Solution - Solution Score Help</strong>
+            <p>If you're asking why ChoozeE gets you to list the objectives before you've even discussed the options, here's the reason why...</p>
+            <p>As human beings, we tend to jump far too quickly into solutions mode. We begin to visualise the answer before we've taken the time to understand why we need one, and who is going to be affected by our decision. By thinking first about the outcomes we are striving for, we should avoid missing important considerations along the way.</p>
+            <p>Your list of options may be short, for example "Do it" or "Don't do it", OR you may want to include options that span a range of interventions. Include specifics wherever possible.</p>
+            <p>Here's an example of the options to address MCH service shortages in a regional town:</p>
+            <ul>
+                <li>Maintain the existing building and extend opening hours</li>
+                <li>Extend the existing building (conditional car parking assessment)</li>
+                <li>Maintain the existing building and lease additional space at 13 Easy Street</li>
+                <li>Maintain the existing building and lease additional space at 50 Peters Road</li>
+                <li>Redesign the existing building with option to add a second floor</li>
+                <li>Repurpose/refurbish the old library building and close the existing building</li>
+                <li>Select a single development site in a central location</li>
+                <li>Select 2 development sites to best meet highest demand</li>
+            </ul> 
+            <p>All options must be viable, and have conditions clearly stated (as in option 2).</p>   
         </div>
     )
 }
