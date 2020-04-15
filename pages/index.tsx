12e6aba41614a295a9a432f4ad57d0859424b3a1
“Son <son.chau@geografia.com.au>
2020-04-16 08:18:16 +1000

    [#49] remove cookies on page load

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,16 @@ import React, {useEffect} from 'react';
 import { NextPage } from 'next';
 import { withApollo } from '../lib/apollo';
 import Dashboard from '../components/Dashboard';
-
+import Cookie from 'js-cookie';
+import { COOKIES } from "../services/cookie";
 interface InitialProps {}
 
 interface Props extends InitialProps {}
 
 const IndexPage: NextPage<Props, InitialProps> = props => {
-  //localStorage.removeItem('issue')
+  //localStorage.removeItem('issueId')
+  Cookie.remove(COOKIES.user)
+  Cookie.remove(COOKIES.name)
   return (
       <Dashboard />
   );
